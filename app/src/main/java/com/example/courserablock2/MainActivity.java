e2502745afcbc8154494eeee5187336e0f096d39
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-08 17:21:38 +0530

    Created ArrayAdapter for animals

--- app/src/main/java/com/example/courserablock2/MainActivity.java
+++ app/src/main/java/com/example/courserablock2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.courserablock2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView animalList = findViewById(R.id.animalListView);
+        ArrayAdapter<CharSequence> animalAdapter = ArrayAdapter.createFromResource(this, R.array.animalsArray,
+                android.R.layout.simple_list_item_1);
     }
 }
